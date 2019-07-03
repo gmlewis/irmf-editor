9b646a8dd3069605c77e88b7f61e0e742b31c564
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-07-03 07:16:25 -0400

    Start parsing URLs

--- main.go
+++ main.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/gowebapi/webapi"
+	"github.com/gowebapi/webapi/core/js"
+)
+
+var (
+	window *webapi.Window
+	editor interface{}
 )
 
 func main() {
-	// document := js.Global().Get("document")
-	// body := document.Call("querySelector", "body")
+	window = webapi.GetWindow()
+	editor = js.Global().Get("editor")
 
-	// div := document.Call("createElement", "div")
-	// div.Set("textContent", "Welcome in your wasm app irmf-editor")
+	loadSource()
+
+	// element := window.Document().GetElementById("foo")
+	// button := html.HTMLButtonElementFromJS(element)
+	// button.SetInnerText("Press me!")
 	//
-	// body.Call("appendChild", div)
+	// count := 1
+	// callback := domcore.EventHandlerToJS(func(event *domcore.Event) interface{} {
+	// 	button.SetInnerText(fmt.Sprint("Count: ", count))
+	// 	count++
+	// 	return nil
+	// })
+	// button.SetOnclick(callback)
 
 	fmt.Println("Application irmf-editor is now started")
+
+	// prevent program from terminating
+	c := make(chan struct{}, 0)
+	<-c
+}
+
+func loadSource() {
+	url := window.Location().Value_JS.String()
+	i := strings.Index(url, "/?s=github.com/")
+	if i < 0 {
+		return
+	}
+	location := url[i+4:]
+	lower := strings.ToLower(location)
+	if !strings.HasSuffix(lower, ".irmf") {
+		window.Alert2("irmf-editor will only load .irmf files")
+		return
+	}
+
+	fmt.Printf("location=%q\n", location)
+	fmt.Printf("editor=%#v\n", editor)
 }
