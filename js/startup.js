83dbfcdb53582c8a1169ddf28cc91d5d69a327e5
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-07-04 18:29:06 -0400

    Work on trackball

--- js/startup.js
+++ js/startup.js
@@ -122,4 +122,24 @@ var animate = function () {
   renderer.render(scene, camera);
 };
 
-animate();
+// animate();
+
+var render = function () {
+  renderer.render(scene, camera);
+};
+
+var controls = new TrackballControls(camera, canvas);
+
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+
+controls.noZoom = false;
+controls.noPan = false;
+
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+
+controls.keys = [65, 83, 68];
+
+controls.addEventListener('change', render);
