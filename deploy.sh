09d6f0b51be1aa3264e50c1ef589ac3c45143c4c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-07-06 20:22:25 -0400

    Tweak deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git checkout gh-pages
 git merge master
 # tinygo build -o main.wasm -target wasm ./main.go
 GOARCH=wasm GOOS=js go build -o main.wasm
-git commit -am 'Update wasm'
+git commit -am 'Update wasm' || echo ''
 git push origin gh-pages
 git checkout master
