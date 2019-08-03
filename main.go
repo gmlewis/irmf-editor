20c9acb7af6f0485498f6d14acf34da4b90ada39
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-08-03 14:14:11 -0400

    Start full color support

--- main.go
+++ main.go
@@ -150,15 +150,124 @@ func initShader(src []byte) interface{} {
 	}
 
 	// Set the GUI to the correct number of materials and their color editors.
+	// Also support HSV, HSL, and RGB full color models. If any material is
+	// listed three times with unique suffix triplets ('.H','.S','.V'),
+	// ('.H','.S','.L'), or ('.R','.G','.B'), they are combined to form a
+	// color in the editor using the appropriate color space model.
+	// See https://en.wikipedia.org/wiki/HSL_and_HSV for more information.
+	hsvs := hsvMap{}
+	hsls := hslMap{}
+	rgbs := rgbMap{}
 	colorFolder := js.Global().Call("getColorFolder")
 	if colorFolder.Type() != js.TypeNull && colorFolder.Type() != js.TypeUndefined {
+		// TODO: Only count the colors for non-full-color materials.
 		colorFolder.Set("name", fmt.Sprintf("Material colors (%v)", len(jsonBlob.Materials)))
 		jsArray := js.ValueOf([]interface{}{})
+		setH := func(name string, colorNum int) {
+			if v, ok := hsvs[name]; ok {
+				v.H = colorNum
+			} else {
+				hsvs[name] = hsvT{H: colorNum}
+			}
+			if v, ok := hsls[name]; ok {
+				v.H = colorNum
+			} else {
+				hsls[name] = hslT{H: colorNum}
+			}
+		}
+		setS := func(name string, colorNum int) {
+			if v, ok := hsvs[name]; ok {
+				v.S = colorNum
+			} else {
+				hsvs[name] = hsvT{S: colorNum}
+			}
+			if v, ok := hsls[name]; ok {
+				v.S = colorNum
+			} else {
+				hsls[name] = hslT{S: colorNum}
+			}
+		}
+		setV := func(name string, colorNum int) {
+			if v, ok := hsvs[name]; ok {
+				v.V = colorNum
+			} else {
+				hsvs[name] = hsvT{V: colorNum}
+			}
+		}
+		setL := func(name string, colorNum int) {
+			if v, ok := hsls[name]; ok {
+				v.L = colorNum
+			} else {
+				hsls[name] = hslT{L: colorNum}
+			}
+		}
+		setR := func(name string, colorNum int) {
+			if v, ok := rgbs[name]; ok {
+				v.R = colorNum
+			} else {
+				rgbs[name] = rgbT{R: colorNum}
+			}
+		}
+		setG := func(name string, colorNum int) {
+			if v, ok := rgbs[name]; ok {
+				v.G = colorNum
+			} else {
+				rgbs[name] = rgbT{G: colorNum}
+			}
+		}
+		setB := func(name string, colorNum int) {
+			if v, ok := rgbs[name]; ok {
+				v.B = colorNum
+			} else {
+				rgbs[name] = rgbT{B: colorNum}
+			}
+		}
 		for i, name := range jsonBlob.Materials {
 			jsArray.SetIndex(i, name)
+			if len(name) > 2 {
+				baseName := name[0 : len(name)-2]
+				switch {
+				case strings.HasSuffix(name, ".H"): // Make entries for both HSV and HSL, then clean up later.
+					setH(name, i+1)
+				case strings.HasSuffix(name, ".S"): // Make entries for both HSV and HSL, then clean up later.
+					setS(name, i+1)
+				case strings.HasSuffix(name, ".V"):
+					setV(name, i+1)
+				case strings.HasSuffix(name, ".L"):
+					setL(name, i+1)
+				case strings.HasSuffix(name, ".R"):
+					setR(name, i+1)
+				case strings.HasSuffix(name, ".G"):
+					setG(name, i+1)
+				case strings.HasSuffix(name, ".B"):
+					setB(name, i+1)
+				}
+			}
 		}
 		js.Global().Call("refreshMaterialColorControllers", jsArray)
 	}
+
+	// Remove incomplete color models.
+	cleanMap := func(keys []string, removeFunc func(key string)) {
+		for _, k := range keys {
+			removeFunc(k)
+		}
+	}
+	cleanMap(hsvs.keys(), func(key string) {
+		if hsvs[key].H == 0 || hsvs[key].S == 0 || hsvs[key].V == 0 {
+			delete(hsvs, key)
+		}
+	})
+	cleanMap(hsls.keys(), func(key string) {
+		if hsls[key].H == 0 || hsls[key].S == 0 || hsls[key].L == 0 {
+			delete(hsls, key)
+		}
+	})
+	cleanMap(rgbs.keys(), func(key string) {
+		if rgbs[key].R == 0 || rgbs[key].G == 0 || rgbs[key].B == 0 {
+			delete(rgbs, key)
+		}
+	})
 
 	// Set the updated MBB:
 	rangeValues := js.Global().Call("getRangeValues")
@@ -178,9 +287,66 @@ func initShader(src []byte) interface{} {
 	}
 
 	// logf("Compiling new model shader:\n%v", shaderSrc)
-	js.Global().Call("loadNewModel", shaderSrc+fsFooter(len(jsonBlob.Materials)))
+	js.Global().Call("loadNewModel", shaderSrc+fsFooter(len(jsonBlob.Materials), hsvs, hsls, rgbs))
 
 	return nil
+}
+
+type hsvT struct {
+	H int
+	S int
+	V int
+}
+
+type hslT struct {
+	H int
+	S int
+	L int
+}
+
+type rgbT struct {
+	R int
+	G int
+	B int
+}
+
+// hsvMap maps a material prefix name (e.g. "PLA") to the material numbers
+// (1-based index) for each of its components. So if the materials were:
+// ["metal", "PLA.V", "dielectric", "PLA.H", "PLA.S"], then the map entry
+// would be: "PLA": {H: 4, S: 5, V: 2}.
+type hsvMap map[string]hsvT
+
+func (m hsvMap) keys() (result []string) {
+	for k := range m {
+		result = append(result, k)
+	}
+	return result
+}
+
+// hslMap maps a material prefix name (e.g. "PLA") to the material numbers
+// (1-based index) for each of its components. So if the materials were:
+// ["metal", "PLA.L", "dielectric", "PLA.H", "PLA.S"], then the map entry
+// would be: "PLA": {H: 4, S: 5, L: 2}.
+type hslMap map[string]hslT
+
+func (m hslMap) keys() (result []string) {
+	for k := range m {
+		result = append(result, k)
+	}
+	return result
+}
+
+// rgbMap maps a material prefix name (e.g. "PLA") to the material numbers
+// (1-based index) for each of its components. So if the materials were:
+// ["metal", "PLA.B", "dielectric", "PLA.R", "PLA.G"], then the map entry
+// would be: "PLA": {R: 4, G: 5, B: 2}.
+type rgbMap map[string]rgbT
+
+func (m rgbMap) keys() (result []string) {
+	for k := range m {
+		result = append(result, k)
+	}
+	return result
 }
 
 func parseEditor(src []byte) (*irmf, string) {
@@ -403,41 +569,101 @@ void mainModel4( out vec4 materials, in vec3 xyz ) {
 }
 `
 
-func fsFooter(numMaterials int) string {
+func fsFooter(numMaterials int, hsvs hsvMap, hsls hslMap, rgbs rgbMap) string {
+	if len(hsvs) == 0 && len(hsls) == 0 && len(rgbs) == 0 {
+		switch numMaterials {
+		default:
+			return fmt.Sprintf(fsFooterFmt4, "u_d*u_color1*m.x")
+		case 2:
+			return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y)")
+		case 3:
+			return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y + u_color3*m.z)")
+		case 4:
+			return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y + u_color3*m.z + u_color4*m.w)")
+		case 5:
+			return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1])")
+		case 6:
+			return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2])")
+		case 7:
+			return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0])")
+		case 8:
+			return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0] + u_color8*m[2][1])")
+		case 9:
+			return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0] + u_color8*m[2][1] + u_color9*m[2][2])")
+		case 10:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1])")
+		case 11:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2])")
+		case 12:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3])")
+		case 13:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0])")
+		case 14:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1])")
+		case 15:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1] + u_color15*m[2][2])")
+		case 16:
+			return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1] + u_color15*m[2][2] + u_color16*m[2][3])")
+		}
+	}
+
+	var colorToMaterial func(colorNum int) string
+	var footerFmt string
 	switch numMaterials {
 	default:
-		return fmt.Sprintf(fsFooterFmt4, "u_d*u_color1*m.x")
-	case 2:
-		return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y)")
-	case 3:
-		return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y + u_color3*m.z)")
-	case 4:
-		return fmt.Sprintf(fsFooterFmt4, "u_d*(u_color1*m.x + u_color2*m.y + u_color3*m.z + u_color4*m.w)")
-	case 5:
-		return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1])")
-	case 6:
-		return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2])")
-	case 7:
-		return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0])")
-	case 8:
-		return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0] + u_color8*m[2][1])")
-	case 9:
-		return fmt.Sprintf(fsFooterFmt9, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[1][0] + u_color5*m[1][1] + u_color6*m[1][2] + u_color7*m[2][0] + u_color8*m[2][1] + u_color9*m[2][2])")
-	case 10:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1])")
-	case 11:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2])")
-	case 12:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3])")
-	case 13:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0])")
-	case 14:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1])")
-	case 15:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1] + u_color15*m[2][2])")
-	case 16:
-		return fmt.Sprintf(fsFooterFmt16, "u_d*(u_color1*m[0][0] + u_color2*m[0][1] + u_color3*m[0][2] + u_color4*m[0][3] + u_color5*m[1][0] + u_color6*m[1][1] + u_color7*m[1][2] + u_color8*m[1][3] + u_color9*m[2][0] + u_color10*m[2][1] + u_color11*m[2][2] + u_color12*m[2][3] + u_color13*m[2][0] + u_color14*m[2][1] + u_color15*m[2][2] + u_color16*m[2][3])")
+		footerFmt = fsFooterFmt4
+		colorToMaterial = func(colorNum int) string {
+			return []string{"m.x", "m.y", "m.z", "m.z"}[colorNum-1]
+		}
+	case 5, 6, 7, 8, 9:
+		footerFmt = fsFooterFmt9
+		colorToMaterial = func(colorNum int) string {
+			return []string{"m[0][0]", "m[0][1]", "m[0][2]", "m[1][0]", "m[1][1]", "m[1][2]", "m[2][0]", "m[2][1]", "m[2][2]"}[colorNum-1]
+		}
+	case 10, 11, 12, 13, 14, 15, 16:
+		footerFmt = fsFooterFmt16
+		colorToMaterial = func(colorNum int) string {
+			return []string{"m[0][0]", "m[0][1]", "m[0][2]", "m[0][3]", "m[1][0]", "m[1][1]", "m[1][2]", "m[1][3]", "m[2][0]", "m[2][1]", "m[2][2]", "m[2][3]", "m[2][0]", "m[2][1]", "m[2][2]", "m[2][3]"}[colorNum-1]
+		}
 	}
+
+	usedColors := map[int]bool{}
+	var finalColors []string
+	for _, v := range hsvs {
+		usedColors[v.H] = true
+		usedColors[v.S] = true
+		usedColors[v.V] = true
+		finalColors = append(finalColors, fmt.Sprintf("hsv(%v,%v,%v)", colorToMaterial(v.H), colorToMaterial(v.S), colorToMaterial(v.V)))
+	}
+	for _, v := range hsls {
+		usedColors[v.H] = true
+		usedColors[v.S] = true
+		usedColors[v.L] = true
+		finalColors = append(finalColors, fmt.Sprintf("hsl(%v,%v,%v)", colorToMaterial(v.H), colorToMaterial(v.S), colorToMaterial(v.L)))
+	}
+	for _, v := range rgbs {
+		usedColors[v.R] = true
+		usedColors[v.G] = true
+		usedColors[v.B] = true
+		finalColors = append(finalColors, fmt.Sprintf("vec4(%v,%v,%v,1.0)", colorToMaterial(v.R), colorToMaterial(v.G), colorToMaterial(v.B)))
+	}
+	nextColor := 1
+	for i := 1; i <= numMaterials; i++ {
+		if !usedColors[i] {
+			finalColors = append(finalColors, fmt.Sprintf("u_color%v*%v", nextColor, colorToMaterial(i)))
+			nextColor++
+		}
+	}
+
+	var prefixFuncs []string
+	if len(hsvs) > 0 {
+		prefixFuncs = append(prefixFuncs, hsvFunc)
+	}
+	if len(hsls) > 0 {
+		prefixFuncs = append(prefixFuncs, hslFunc)
+	}
+	colorMixer := fmt.Sprintf("u_d*(%v)", strings.Join(finalColors, " + "))
+	return strings.Join(prefixFuncs, "\n") + fmt.Sprintf(footerFmt, colorMixer)
 }
 
 const fsFooterFmt4 = `
@@ -494,5 +720,30 @@ void main() {
 	mainModel16(m, v_xyz.xyz);
 	out_FragColor = %v;
 	// out_FragColor = v_xyz/5.0 + 0.5;  // DEBUG
+}
+`
+
+const hsvFunc = `
+vec4 hsv(float h, float s, float v) {
+  float k5 = mod(5.0+6.0*h, 6.0);
+  float k3 = mod(3.0+6.0*h, 6.0);
+  float k1 = mod(1.0+6.0*h, 6.0);
+  float f5 = v - v*s*max(min(k5,4.0-k5,1.0),0.0);
+  float f3 = v - v*s*max(min(k3,4.0-k3,1.0),0.0);
+  float f1 = v - v*s*max(min(k1,4.0-k1,1.0),0.0);
+  return vec4(f5,f3,f1,1.0);
+}
+`
+
+const hslFunc = `
+vec4 hsl(float h, float s, float l) {
+  float a = s*min(l,1.0-l);
+  float k0 = mod(0.0+12.0*h, 12.0);
+  float k8 = mod(8.0+12.0*h, 12.0);
+  float k4 = mod(4.0+12.0*h, 12.0);
+  float f0 = l - a*max(min(k0-3.0,9.0-k0,1.0),-1.0);
+  float f8 = l - a*max(min(k8-3.0,9.0-k8,1.0),-1.0);
+  float f4 = l - a*max(min(k4-3.0,9.0-k4,1.0),-1.0);
+  return vec4(f0,f8,f4,1.0);
 }
 `
