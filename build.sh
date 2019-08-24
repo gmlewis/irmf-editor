dcf96f98cdd65c9b7f5ff50585d0c8ff4ded7e74
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-08-24 11:52:54 -0400

    Tweak tinygo command line options

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 # tinygo crashes on building this app. Use Go compiler instead.
-# tinygo build -o main.wasm -target wasm ./main.go
+# tinygo build -o main.wasm -target wasm .
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js js
 GOARCH=wasm GOOS=js go build -o main.wasm
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout master
 git push origin master || echo ''
 git checkout gh-pages
 git merge master
-# tinygo build -o main.wasm -target wasm ./main.go
+# tinygo build -o main.wasm -target wasm .
 GOARCH=wasm GOOS=js go build -o main.wasm
 git commit -am 'Update wasm' || echo ''
 git push origin gh-pages
