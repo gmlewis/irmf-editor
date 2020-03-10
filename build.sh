7050ee91cfd08121ec0b5c677c864444f00c9d38
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-03-09 21:48:10 -0400

    Fix Go 1.14 wasm error

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 cat $(go env GOROOT)/misc/wasm/wasm_exec.js |
-    sed -e 's/global.fs = require.*$/try { & } catch (err) {}/' \
+    sed -e 's/global.require.main === module/false/' \
+        -e 's/global.fs = require.*$/try { & } catch (err) {}/' \
     > js/wasm_exec.js
 # tinygo 0.12.0 doesn't build this app without errors. Use the Go compiler instead.
 # tinygo build -o main.wasm -target wasm .
