40e8cda318cc4c5fb0a8bfe19b671172ca7aca40
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-10-26 16:36:59 -0400

    Support https://lygia.xyz (#6)

--- main_test.go
+++ main_test.go
@@ -136,3 +136,43 @@ func TestGenColorMixer(t *testing.T) {
 		})
 	}
 }
+
+func TestParseIncludeURL(t *testing.T) {
+	tests := []struct {
+		name    string
+		trimmed string
+		want    string
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name:    "bogus",
+			trimmed: `#include "bad/include.h"`,
+		},
+		{
+			name:    "normal",
+			trimmed: `#include "lygia/math/decimation.glsl"`,
+			want:    "https://lygia.xyz/math/decimation.glsl",
+		},
+		{
+			name:    "extra space",
+			trimmed: `#include    "lygia/math/decimation.glsl"`,
+			want:    "https://lygia.xyz/math/decimation.glsl",
+		},
+		{
+			name:    "accidental copy/paste",
+			trimmed: `#include "lygia.xyz/math/decimation.glsl"`,
+			want:    "https://lygia.xyz/math/decimation.glsl",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parseIncludeURL(tt.trimmed)
+			if got != tt.want {
+				t.Errorf("parseIncludeURL got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
